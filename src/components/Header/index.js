45784a7ecee47bce251bd5dc3b76272c94cce01f
Adriano Henrique <adriano.henrique.eng@gmail.com>
2021-11-25 23:33:02 -0300

    Adicionados primeiros componentes e estilos

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'style';
+
+const Header = () => {
+    return (
+        <header className="header__container">
+            <div className="header__logo--state">
+                <img src=""/>
+            </div>
+            <div className="header__logo--ebac"></div>
+
+        </header>
+    )
+}
