4a3c36bc09f42ad4567bb5924c19d8eeac6f37d1
Adriano Henrique <adriano.henrique.eng@gmail.com>
2021-11-27 23:42:10 -0300

    Adição, estilização e primeiras ações das Pills via PropTypes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import '../Home/styles.css'
 import imageDestack from '../../images/imagem-destaque.png'
+import Pills from "../../components/Pills";
+
+const PLACES = [
+    'Praça',
+    'Parque',
+    'Igreja',
+    'Hotel',
+    'Restaurante',
+    'Zoológico',
+    'Farmácia',
+    'Loja',
+    'Aquário'
+]
 
 const Home = () => {
+    const [selectedPill, setSelectedPill] = useState('')
     return (
         <main id="main-content" className="home__container">
             <div className="home__col">
@@ -10,12 +24,18 @@ const Home = () => {
                 <p className="home__text">Nossa missão é facilitar o encontro entre lugares inclusivos e pessoas que buscam alternativas culturais para sair de casa, com a mobilidade que todo cidadão precisa.</p>
                 <p className="home__text">Checamos 10 itens essenciais de acessibilidade para pessoas com deficiência em diversos pontos da cidade.</p>
                 <div className="home__pills">
-                    
+                    {PLACES.map((item) => (
+                        <Pills 
+                            local={item}
+                            selected={selectedPill === item}
+                            onClick={() => setSelectedPill(item)}
+                        />
+                    ))}
                 </div>
             </div>
             <div className="home__col">
                 <div className="home__image home__image--destaque">
-                    <img src={imageDestack} alt="Imagem destaque cadeirante"/>
+                    <img src={imageDestack} alt="Imagem destaque cadeirante" />
                 </div>
             </div>
         </main>
