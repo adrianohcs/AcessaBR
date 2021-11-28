29eff1e432a0db6d2ef5637ee7f0931f00d228c6
Adriano Henrique <adriano.henrique.eng@gmail.com>
2021-11-27 21:28:07 -0300

    Criação e primeira estilização da Home

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global.css'
 import Header from './components/Header'
+import Home from './pages/Home';
 
 ReactDOM.render(
   <div className="main__wrapper">
     <div className="main__container">
-      <Header city="Goiânia" state="GO"/>
+      <Header city="Goiânia" state="GO" />
+      <Home />
     </div>
 
   </div>
